4699f4c88d7439018eb5020c98589cf09b3ae5b7
Andranik Arakelov <andranik.arakelov@etu.unice.fr>
2023-01-05 13:10:38 +0100

    dz

---
+++ error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mt-7 max-w-sm mx-auto text-center card">
+      <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
+      <p class="mt-7 text-6xl font-bold">Ooops</p>
+      <p class="mt-7">{{error.message}}</p>
+      <button class="btn mt-7" @click="goBack">Go back</button>
+    </div>
+  </template>
+  
+<script setup>
+  const goBack = () => clearError({redirect: "/"})
+  defineProps(["error"])
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+         <header class="shadow-sm bg-white">
+         <nav class="container mx-auto p-4 flex justify-between">
+             <NuxtLink to="/" class="font-bold">EC</NuxtLink>
+         </nav>
+
+         </header>
+         <nav class="container mx-auto flex justify-center">
+            <ul class="p-2" v-for="category in categories" :key="category">
+                 <li><NuxtLink :to="`/products/${category}`">{{category}}</NuxtLink></li>
+             </ul>
+        </nav>
+       <div class="container mx-auto p-4">
+         <slot />
+       </div>
+       <footer class="container mx-auto p-4 flex justify-between border-t-2">
+             <ul class="flex gap-4">
+                 <li><NuxtLink to="/about">About</NuxtLink></li>
+             </ul>
+         </footer>
+     </div>
+  </template>
+  
+  <script setup lang="ts">
+     const {id} = useRoute().params
+    //  const category_selected = ref(id)
+     const {data:categories} = await useFetch("https://fakestoreapi.com/products/categories")
+  </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
